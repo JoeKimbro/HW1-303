0bad0798f9e8b78f66b4eaca5bb3b1d7934c86f5
JoeKimbro <159492219+JoeKimbro@users.noreply.github.com>
2024-02-08 23:02:10 -0600

    Delete Header.h

--- Header.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include <stdbool.h>
-#include <fstream>
-#include <algorithm>
-using namespace std;
-const int arraySize = 100; int dataArray[arraySize];
-void readFromArray(const string& fileName);
-void replaceAtIndex(int index, bool remove);
